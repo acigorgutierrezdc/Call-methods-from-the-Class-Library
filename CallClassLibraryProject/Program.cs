cada0e11fa18657ab6c09ddff59fbf676260fd49
IGOR GUTIERREZ <ac.igor.gutierrez.dc@gmail.com>
2024-02-20 17:14:44 -0300

    Updates.

---
+++ CallClassLibraryProject/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+
+        // Random dice = new Random();
+        // int roll = dice.Next();
+        // Console.WriteLine(roll);
+
+        // Random dice = new Random();
+        // int roll1 = dice.Next();
+        // int roll2 = dice.Next(101);
+        // int roll3 = dice.Next(50, 101);
+
+        // Console.WriteLine($"First roll: {roll1}");
+        // Console.WriteLine($"Second roll: {roll2}");
+        // Console.WriteLine($"Third roll: {roll3}");
+        // dice.Next()
+
+        int firstValue = 500;
+        int secondValue = 600;
+        int largerValue;
+
+        
+
+        Console.WriteLine(System.Math.Max(firstValue,secondValue));
+
+
+
+    }
+}
